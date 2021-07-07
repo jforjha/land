d5e6e792c113e1dc799170116ba3685fa71012d5
vidhitkathuria <72202024+vidhitkathuria@users.noreply.github.com>
2021-07-07 16:35:32 +0530

    installed firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+// import firebase from "firebase";
 
-createApp(App).use(router).mount('#app')
+// var firebaseConfig = {
+//   apiKey: "AIzaSyD90cWJkgMsjc26RK2NmbDMvdzUovPm780",
+//   authDomain: "auth-495b7.firebaseapp.com",
+//   projectId: "auth-495b7",
+//   storageBucket: "auth-495b7.appspot.com",
+//   messagingSenderId: "838397777574",
+//   appId: "1:838397777574:web:97f23bb497373f082a4667",
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+
+createApp(App)
+  .use(router)
+  .mount("#app");
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,18 +1,50 @@
 <template>
-<div class="dashboard">
-  <h1>Login Page</h1>
-  <router-link class="link1" :to="{ name: 'DashBoard' }"
-    >User DashBoard</router-link
-  >
-  </div>
+<div class="container">
+    <div class="col-md-3">Left</div>
+    <div class="col-md-3">Mid</div>
+    <div class="col-md-3">Right</div>
+</div>
+  <!-- <div>
+    <form @submit.prevent="login" class="form">
+      <div class="details">
+        <h2 class="h2">Login</h2>
+        <input
+          class="email"
+          type="email"
+          placeholder="Email address..."
+          v-model="email"
+        />
+        <input
+          type="password"
+          placeholder="password..."
+          v-model="password"
+          class="password"
+        />
+        <button type="submit" class="button">
+          Login
+        </button>
+      </div>
+    </form>
+  </div> -->
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
-.dashboard {
-    
-}
+<style lang="scss" scoped>
+// .form {
+//     margin-top: 300px;
+//   display: flex;
+//   border: solid 1px black;
+//   justify-content: center;
+//   .details{
+//    display: flex;
+//    flex-direction: column;
+//    border: solid 1px black;
+//    height: 200px;
+//    width: 200px;
+//    border-radius: 20px;
+//   }
+// }
 </style>
